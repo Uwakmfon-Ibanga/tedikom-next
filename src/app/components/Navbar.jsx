6b9d40f30db25821e145d5d152ee779575117077
Uwakmfon-Ibanga <ibangauwakmfon200@gmail.com>
2025-02-06 00:05:44 +0100

    removed @headlessui/react import statements

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,5 +1,4 @@
-import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
-// import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+
 import { LuMenu, LuX } from 'react-icons/lu'
 import BorderBotton from './Buttons/BorderBotton'
 
